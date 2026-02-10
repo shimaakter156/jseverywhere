ac30a0e7958d8224776a10242176540f8a9718ed
MIS-WD-Shima Akter <Shima@aci-bd.com>
2026-02-10 14:12:13 +0600

    console

---
+++ consoletypes.js
@@ -0,0 +1,30 @@
+//groupconsole
+//const users = [
+//     {name:'sdsd',age:25},
+//         {name:'erer',age:58},
+//     ]
+// console.table(users)
+// console.log(users)
+// console.log(JSON.stringify(users,null,2))
+//
+// console.group('user details')
+// console.log('name',':','shima')
+// console.log('age',':',25)
+// console.groupEnd();
+//
+//
+// const employee = {
+//     name:'john',
+//     age:30,
+//     address:{
+//         city:'nyc',
+//         zip:'101'
+//     }
+// }
+
+// // all are same
+// console.log('helo')
+// console.info('info')
+// console.warn('warn')
+// console.error('error')
+// console.debug('debug')
