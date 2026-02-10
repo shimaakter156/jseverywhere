01c989b8b99c58f2f157e53e194fa115b6441ad9
MIS-WD-Shima Akter <Shima@aci-bd.com>
2026-02-10 14:12:37 +0600

    console

--- consoletypes.js
+++ consoletypes.js
@@ -1,4 +1,37 @@
-//groupconsole
+// //groupconsole
+//
+// const employee = {
+//     name:'john',
+//     age:30,
+//     address:{
+//         city:'nyc',
+//         zip:'101'
+//     }
+// }
+//
+// console.group('Outer Group');
+// console.log('Outer message');
+// console.group('Inner Group');
+// console.log('Inner message');
+// console.groupEnd();
+// console.groupEnd();
+//
+// console.group('User Details');
+// console.log('Name: John');
+// console.log('Age: 30');
+// console.log('City: NYC');
+// console.groupEnd();
+//
+// console.groupCollapsed('sdsdsd')
+// console.log('sdsdsd','sdsd')
+// console.log('sdsdsd','sdsd')
+// console.groupEnd();
+//
+// //the thml can be call by its id
+// // const element = document.querySelector('#mydiv')
+// // console.dirxml(element)
+// console.dir(employee,{depth:null})
+// console.table(employee)
 //const users = [
 //     {name:'sdsd',age:25},
 //         {name:'erer',age:58},
