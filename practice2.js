fbf271e4d1429a6e17462ba52f9fec4db295d0d6
MIS-WD-Shima Akter <SHIMA@ACI-BD.COM>
2026-02-10 09:35:34 +0600

    dsdsd

---
+++ practice2.js
@@ -0,0 +1,59 @@
+// function return in function
+// function compareBy(propertyName){
+//     return function(a,b){
+//         let x = a[propertyName],
+//             y = b[propertyName];
+//
+//         if(x>y){
+//             return 1
+//         }else if(x<y){
+//             return -1
+//         }else{
+//             return 0;
+//         }
+//     }
+// }
+
+
+// function add(a,b){
+//     return a+b
+// }
+//
+// function average(a,b,fn){
+//     return fn(a,b)/2;
+// }
+// let sum = add;
+//
+// let result = average(1,2,sum)
+// console.log(result);
+
+// function square(x){
+//     x = x*x
+//     return x
+// }
+// let y=10;
+// let result = square(y)
+// console.log(result);
+
+// const as = 5
+// console.log(const as=6);
+//
+//
+// var a = 20;
+// var b=3
+// let a1 = 1;
+// let b1= 2
+// function addvalues(a,b){
+//     return a+b
+// }
+// let result = addvalues(1,2)
+//
+// let item = [1,2,3]
+//
+// for( let i = 0;i<item.length;i++){
+//
+// }
+//
+// for(let i of item){
+//     // console.log('bvbv');
+// }
